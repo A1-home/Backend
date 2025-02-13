8e383cc79adeb231cf08b82383187ff389c2fc1a
Sonu3w <sonu@triplewsols.com>
2025-02-14 00:03:48 +0530

    done till preview setting

--- src/main/java/com/example/Security/SecurityApplication.java
+++ src/main/java/com/example/Security/SecurityApplication.java
@@ -9,13 +9,7 @@ public class SecurityApplication {
 
 	public static void main(String[] args) {
 		// Load environment variables from .env file
-		Dotenv dotenv = Dotenv.load();
 
-		// Set environment variables as system properties
-		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-		System.setProperty("AWS_S3_BUCKET_NAME", dotenv.get("AWS_S3_BUCKET_NAME"));
-		System.setProperty("AWS_S3_REGION", dotenv.get("AWS_S3_REGION"));
 
 		SpringApplication.run(SecurityApplication.class, args);
 	}
